3197f60a693069193dbb646e28b3e54d6fa34a5b
Cuong_LT <letrungcuong2k@gmail.com>
2021-01-25 20:52:32 +0700

    User Lê Trung Cương edited

--- services/BaseService.js
+++ services/BaseService.js
@@ -1,54 +1,44 @@
 export class BaseService {
+  constructor() {}
 
-    constructor(){
+  post = (url, data) => {
+    const promise = axios({
+      url: url,
+      method: "POST",
+      data: data,
+    });
 
-    }
+    return promise;
+  };
 
-    post = (url,data) => {
-        const promise = axios ({
-            url: url,
-            method: 'POST',
-            data:data
-        })
+  get = (url) => {
+    const promise = axios({
+      url: url,
+      method: "GET",
+    });
 
-        return promise;
-    } 
+    return promise;
+  };
 
-    get = (url) => {
-        const promise = axios ({
-            url: url,
-            method: 'GET'
-        })
+  put = (url, data) => {
+    const promise = axios({
+      url: url,
+      method: "PUT",
+      data: data,
+    });
 
-        return promise;
-    } 
+    return promise;
+  };
 
-    put = (url,data) => {
-        const promise = axios ({
-            url: url,
-            method: 'PUT',
-            data:data
-        })
+  delete = (url) => {
+    const promise = axios({
+      url: url,
+      method: "DELETE",
+    });
 
-        return promise;
-    } 
-
-    delete = (url) => {
-        const promise = axios ({
-            url: url,
-            method: 'DELETE'
-        })
-
-        return promise;
-    } 
+    return promise;
+  };
 }
 
-<<<<<<< HEAD
 console.log("Mình là User Lê Trung Cương");
 console.log("hehe hoho");
-=======
-console.log('Hi All');
-console.log('Hi All');
-console.log('Hi All');
-console.log('Hi All');
->>>>>>> 299f80e846503ce1a7162e541c95e327f5d3ed68
